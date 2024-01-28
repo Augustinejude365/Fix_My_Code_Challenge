b1d16328230614cb6023a305f8f372c85dd4b588
augustinejude <divinefavour4jude@gmail.com>
2024-01-28 13:38:21 -0800

    updated the ruby file

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -1,31 +1,28 @@
-###
-#
-#  Sort integer arguments (ascending) 
-#
-###
+# frozen_string_literal: true
 
+# Sort integer arguments (ascending)
 result = []
 ARGV.each do |arg|
-    # skip if not integer
-    next if arg !~ /^-?[0-9]+$/
+  # Skip if not integer
+  next unless arg.match?(/^-?[0-9]+$/)
 
-    # convert to integer
-    i_arg = arg.to_i
+  # Convert to integer
+  i_arg = arg.to_i
 
-    # insert result at the right position
-    is_inserted = false
-    i = 0
-    l = result.size
-    while !is_inserted && i < l do
-      if result[i] < i_arg
-        i += 1
-      else
-        result.insert(i - 1, i_arg)
-        is_inserted = true
-        break
-      end
+  # Insert result at the right position
+  is_inserted = false
+  i = 0
+  l = result.size
+  while !is_inserted && i < l
+    if result[i] < i_arg
+      i += 1
+    else
+      result.insert(i, i_arg)
+      is_inserted = true
+      break
     end
-    result << i_arg if !is_inserted
+  end
+  result << i_arg unless is_inserted
 end
 
 puts result
